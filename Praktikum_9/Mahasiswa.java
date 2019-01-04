9cead860686b956a78087503492029a14ed06135
ermaokta <36910752+ermaokta@users.noreply.github.com>
2019-01-04 18:09:30 +0700

    Add files via upload

---
+++ Praktikum_9/Mahasiswa.java
@@ -0,0 +1,27 @@
+package Praktikum_9;
+
+
+public class Mahasiswa {
+    int nim;
+    String nama_mhs;
+    
+    public Mahasiswa(int n){
+        
+    }
+    
+    public void setNim(int ni){
+        this.nim=ni;
+    }
+    
+    public int getNim(){
+        return nim;
+    }
+    
+    public void setNama(String na){
+        this.nama_mhs=na;
+    }
+    
+    public String getNama(){
+        return nama_mhs;
+    }
+}
